a8ab6a7ca53ab1e91c4be8f2636289b3e8ae70ac
fakeuserpl <monika7117@gmail.com>
2021-06-12 19:32:31 +0200

    lab 05

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from AzureDB import AzureDB
+from flask_dance.contrib.github import make_github_blueprint, github
+import secrets
+import os
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+github_blueprint = make_github_blueprint(
+    client_id="818a9e110420c482eb69",
+    client_secret="91ee0a85d576d15600e21a3f8e7a5ee084722b56"
+)
+app.register_blueprint(github_blueprint, url_prefix='/login')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    else:
+        account_info = github.get('/user')
+        if account_info.ok:
+            account_info_json = account_info.json()
+            return render_template('index.html')
+    return '<h1>Request failed!</h1>'
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/gallery')
 def gallery():
@@ -19,6 +41,7 @@ def gallery():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 @app.route('/guestbook')
 def guestbook():
